152c96c0b02c071a0b7d6db5c0fb1c843190b87e
salva <salvanakamoto@protonmail.com>
2023-05-23 19:59:39 -0400

    Make grid responsive

--- components/providers-grid.tsx
+++ components/providers-grid.tsx
@@ -47,7 +47,7 @@ const ProvidersGrid = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {providers.map((provider) => (
         <ProviderCard
           provider={provider}
